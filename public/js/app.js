1d2fac31bbd933f0d690de57893f88599aafdff7
devlim <e-hub@E-HUBs-iMac.local>
2015-06-23 17:50:17 +0800

    update

--- public/js/app.js
+++ public/js/app.js
@@ -9,11 +9,30 @@ var Contact = Backbone.Model.extend({
 });
 
 var Contants = Backbone.Collection.extend({
-	model: Contant;
+	model: Contact
 });
 
 var ContactView = Backbone.View.extend({
 	initialize: function(){
-		var template;
+		this.template = _.template($('#ContactView').html());
+	},
+	render: function(){
+		var renderContent = this.template(this.model.toJSON());
+		$(this.el).html(renderContent);
+		return this;
+	}
+});
+
+var ContactsView = Backbone.View.extend({
+	tagName:'section',
+	className: 'row',
+	render: function(){
+		var thisEl = $(this.el)
+		this.collection.each(function(m){
+			var view = new ContactView({model:m});
+			thisEl.append(view.render().el);
+		});
+
+		return this;
 	}
 });
